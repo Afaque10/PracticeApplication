576a5a043797d17237319441d6d94e6336e930b7
Ebadparkar <ebadparkar1911@gmail.com>
2023-05-17 07:13:11 -0700

    splashcreen

--- app/src/main/java/com/example/practiceapplication/SplashScreen.java
+++ app/src/main/java/com/example/practiceapplication/SplashScreen.java
@@ -1,13 +1,29 @@
 package com.example.practiceapplication;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    final Handler handler = new Handler(Looper.getMainLooper());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent b = new Intent(SplashScreen.this,Introductionpage.class);
+                startActivity(b);
+
+            }
+        },1500);
+
+
     }
 }
